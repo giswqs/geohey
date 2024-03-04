98780b5464f8d46c65b8c8db415abdf3f617e443
Qiusheng Wu <giswqs@gmail.com>
2024-03-04 14:40:36 -0500

    Add Map class (#6)

--- geohey/geohey.py
+++ geohey/geohey.py
@@ -1,1 +1,10 @@
 """Main module."""
+
+import ipyleaflet
+
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        self.add_control(ipyleaflet.LayersControl())
