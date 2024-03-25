9238e3f3a00f6274a9fffde02c13c2aa67ba8a5f
Qiusheng Wu <giswqs@gmail.com>
2024-03-25 14:18:30 -0400

    Add docstrings

--- geohey/geohey.py
+++ geohey/geohey.py
@@ -5,8 +5,19 @@ from ipyleaflet import basemaps
 
 
 class Map(ipyleaflet.Map):
+    """This is the map class that inherits from ipyleaflet.Map.
+
+    Args:
+        ipyleaflet (Map): The ipyleaflet.Map class.
+    """
 
     def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        """Initialize the map.
+
+        Args:
+            center (list, optional): Set the center of the map. Defaults to [20, 0].
+            zoom (int, optional): Set the zoom level of the map. Defaults to 2.
+        """
         super().__init__(center=center, zoom=zoom, **kwargs)
 
     def add_tile_layer(self, url, name, **kwargs):
@@ -14,7 +25,18 @@ class Map(ipyleaflet.Map):
         self.add(layer)
 
     def add_basemap(self, name):
+        """
+        Adds a basemap to the current map.
 
+        Args:
+            name (str or object): The name of the basemap as a string, or an object representing the basemap.
+
+        Raises:
+            TypeError: If the name is neither a string nor an object representing a basemap.
+
+        Returns:
+            None
+        """
         if isinstance(name, str):
             url = eval(f"basemaps.{name}").build_url()
             self.add_tile_layer(url, name)
@@ -22,10 +44,20 @@ class Map(ipyleaflet.Map):
             self.add(name)
 
     def add_layers_control(self, position="topright"):
+        """Adds a layers control to the map.
+
+        Args:
+            position (str, optional): The position of the layers control. Defaults to "topright".
+        """
         self.add_control(ipyleaflet.LayersControl(position=position))
 
     def add_geojson(self, data, name="geojson", **kwargs):
+        """Adds a GeoJSON layer to the map.
 
+        Args:
+            data (str | dict): The GeoJSON data as a string or a dictionary.
+            name (str, optional): The name of the layer. Defaults to "geojson".
+        """
         import json
 
         if isinstance(data, str):
@@ -42,7 +74,20 @@ class Map(ipyleaflet.Map):
         self.add(layer)
 
     def add_shp(self, data, name="shp", **kwargs):
+        """
+        Adds a shapefile to the current map.
 
+        Args:
+            data (str or dict): The path to the shapefile as a string, or a dictionary representing the shapefile.
+            name (str, optional): The name of the layer. Defaults to "shp".
+            **kwargs: Arbitrary keyword arguments.
+
+        Raises:
+            TypeError: If the data is neither a string nor a dictionary representing a shapefile.
+
+        Returns:
+            None
+        """
         import shapefile
         import json
 
