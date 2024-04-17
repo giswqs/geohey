fb72edc56a4495a9bf69ecf440aa1feb0067eea4
Qiusheng Wu <giswqs@gmail.com>
2024-04-17 14:48:20 -0400

    Add folium module

---
+++ geohey/foliumap.py
@@ -0,0 +1,24 @@
+import folium
+
+
+class Map(folium.Map):
+
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        super().__init__(location=center, zoom_start=zoom, **kwargs)
+
+    def add_raster(self, data, name="raster", **kwargs):
+        """Adds a raster layer to the map.
+
+        Args:
+            data (str): The path to the raster file.
+            name (str, optional): The name of the layer. Defaults to "raster".
+        """
+
+        try:
+            from localtileserver import TileClient, get_folium_tile_layer
+        except ImportError:
+            raise ImportError("Please install the localtileserver package.")
+
+        client = TileClient(data)
+        layer = get_folium_tile_layer(client, name=name, **kwargs)
+        layer.add_to(self)
