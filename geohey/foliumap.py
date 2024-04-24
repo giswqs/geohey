d48db3b1d3ab0868e59c74a2eaabfa306f925e2d
Qiusheng Wu <giswqs@gmail.com>
2024-04-23 21:54:08 -0400

    Add streamlit functions (#28)

--- geohey/foliumap.py
+++ geohey/foliumap.py
@@ -1,4 +1,5 @@
 import folium
+from ipyleaflet import basemaps
 
 
 class Map(folium.Map):
@@ -22,3 +23,66 @@ class Map(folium.Map):
         client = TileClient(data)
         layer = get_folium_tile_layer(client, name=name, **kwargs)
         layer.add_to(self)
+
+    def add_tile_layer(self, url, name, attribution="Custom Tile", **kwargs):
+        """
+        Adds a tile layer to the current map.
+
+        Args:
+            url (str): The URL of the tile layer.
+            name (str): The name of the layer.
+            attribution (str, optional): The attribution text to be displayed for the layer. Defaults to "Custom Tile".
+            **kwargs: Arbitrary keyword arguments for additional layer options.
+
+        Returns:
+            None
+        """
+        layer = folium.TileLayer(tiles=url, name=name, attr=attribution, **kwargs)
+        layer.add_to(self)
+
+    def add_basemap(self, name, overlay=True):
+        """
+        Adds a basemap to the current map.
+
+        Args:
+            name (str or object): The name of the basemap as a string, or an object representing the basemap.
+            overlay (bool, optional): Whether the basemap is an overlay. Defaults to True.
+
+        Raises:
+            TypeError: If the name is neither a string nor an object representing a basemap.
+
+        Returns:
+            None
+        """
+
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name, overlay=overlay)
+        else:
+            name.add_to(self)
+
+    def to_streamlit(self, width=700, height=500):
+        """
+        Converts the map to a streamlit component.
+
+        Args:
+            width (int, optional): The width of the map. Defaults to 700.
+            height (int, optional): The height of the map. Defaults to 500.
+
+        Returns:
+            object: The streamlit component representing the map.
+        """
+
+        from streamlit_folium import folium_static
+
+        return folium_static(self, width=width, height=height)
+
+    def add_layer_control(self):
+        """
+        Adds a layer control to the map.
+
+        Returns:
+            None
+        """
+
+        folium.LayerControl().add_to(self)
